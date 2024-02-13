bcea5d0c2f1c0d2ed036de6f1160c90e49d51af5
Chingsan53 <cly53@csu.fullerton.edu>
2024-02-13 15:51:39 -0800

    update logo for smart solve

--- src/components/category/category.component.jsx
+++ src/components/category/category.component.jsx
@@ -2,7 +2,6 @@ import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
 import "./category.component.scss";
 
-
 const Category = () => {
   return (
     <Fragment>
@@ -10,13 +9,16 @@ const Category = () => {
         <h1>All Utilities</h1>
         <div className="categories-container">
           <Link className="card" to="/currency-converter">
-            
-            <img className="card-image" src="/img/currency.jpg" alt="" />
+            <img
+              className="card-image"
+              src="/img/currency-converter.jpg"
+              alt=""
+            />
             <div className="card-title">Currency Converter</div>
           </Link>
           <Link className="card" to="/tip-calculator">
             <img className="card-image" src="/img/tip-calculator.jpg" alt="" />
-            <div className="card-title">Tips Calculation</div>
+            <div className="card-title">Tips Calculator</div>
           </Link>
           <Link className="card" to="/unit-converter">
             <img className="card-image" src="/img/unit-converter.jpg" alt="" />
@@ -27,16 +29,40 @@ const Category = () => {
             <div className="card-title">Todo List</div>
           </Link>
           <Link className="card" to="/weather-forecast">
-            <img className="card-image" src="/img/weather.jpg" alt="" />
+            <img
+              className="card-image"
+              src="/img/weather-forecast.jpg"
+              alt=""
+            />
             <div className="card-title">Weather Forecast</div>
           </Link>
           <Link className="card" to="/recipe-finder">
-            <img className="card-image" src="/img/recipe.jpg" alt="" />
+            <img className="card-image" src="/img/recipe-finder.jpg" alt="" />
             <div className="card-title">Recipe Finder</div>
           </Link>
           <Link className="card" to="/note-taking">
             <img className="card-image" src="/img/note-taking.jpg" alt="" />
             <div className="card-title">Note Taking</div>
+          </Link>
+          <Link className="card" to="/note-taking">
+            <img className="card-image" src="/img/expense-tracker.jpg" alt="" />
+            <div className="card-title">Expense Tracker</div>
+          </Link>
+          <Link className="card" to="/note-taking">
+            <img className="card-image" src="/img/Fitness-planner.jpg" alt="" />
+            <div className="card-title">Fitness Planner</div>
+          </Link>
+          <Link className="card" to="/note-taking">
+            <img className="card-image" src="/img/Event-planner.jpg" alt="" />
+            <div className="card-title">Event Planner</div>
+          </Link>
+          <Link className="card" to="/note-taking">
+            <img className="card-image" src="/img/travel-planner.jpg" alt="" />
+            <div className="card-title">Travel Planner</div>
+          </Link>
+          <Link className="card" to="/note-taking">
+            <img className="card-image" src="/img/legal-document.jpg" alt="" />
+            <div className="card-title">Legal Document Template</div>
           </Link>
         </div>
       </div>
